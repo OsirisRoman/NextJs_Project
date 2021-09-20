d3188a402d9f5f650d49b4e2321732e690ddef5c
Osiris Roman <osiris.roman@ulinksolution.com>
2021-09-19 19:05:21 -0500

    Adding the MeetupDetails page for a hardcoded item

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://d500.epimg.net/cincodias/imagenes/2019/10/25/lifestyle/1571997676_358300_1571998101_noticia_normal.jpg"
+      title="A first Meetup"
+      address="Some Address 5, 12345 Some City"
+      description="The meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
